e8df3f35df9ac8ab8c56ec3c19d1fa4e4b7df417
reena47124 <reenarathode47@gmail.com>
2026-06-09 18:53:47 +0530

    update

---
+++ 14.py
@@ -0,0 +1,6 @@
+#numpy
+#reshape an array
+import numpy as np
+a = np.arange(12)
+b = a.reshape(3, 4)
+print(b)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+#numpy
+#reshape an array
+import numpy as np
+a = np.arange(12)
+print(a)
