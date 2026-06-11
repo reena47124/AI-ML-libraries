869b271076b1abe3fb35ab7a8ae0286d283de729
reena47124 <reenarathode47@gmail.com>
2026-06-11 19:57:19 +0530

    update

---
+++ 24.py
@@ -0,0 +1,9 @@
+#numpy
+#create arrays containing all zeros,all ones,numbers from 1 to 20.
+import numpy as np
+a0=np.zeros((4,4))
+print(f"array containing all zeors:{a0}")
+a1=np.ones((3,3))
+print(f"array containing all ones:{a1}")
+ax=np.arange(1,21) #ax=np.arange(1,21,1)
+print(f"array containing numbers from 1 to 20:{ax}")
