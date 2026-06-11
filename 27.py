593bb4d02b742b401227ab750f08f92d7414f99e
reena47124 <reenarathode47@gmail.com>
2026-06-11 20:34:01 +0530

    update

---
+++ 27.py
@@ -0,0 +1,8 @@
+#numpy
+#find: shape, size, data type, dimensions of an array.
+import numpy as np
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(f"shape of the array:{a.shape}")
+print(f"size of the array:{a.size}")
+print(f"data type of the array:{a.dtype}")
+print(f"dimensions of the array:{a.ndim}")
