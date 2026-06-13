cbda6882c16d2421b982e797231667a9d991387b
reena47124 <reenarathode47@gmail.com>
2026-06-13 14:54:27 +0530

    update

---
+++ 34.py
@@ -0,0 +1,8 @@
+#numpy
+#count even and odd numbers
+import numpy as np
+arr = np.array([1,2,3,4,5,6,7,8])
+even = np.sum(arr % 2 == 0)
+odd = np.sum(arr % 2 != 0)
+print("Even:", even)
+print("Odd:", odd)
