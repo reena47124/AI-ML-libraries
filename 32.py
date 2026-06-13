dfdc2a8474161a779c833af2b0156f83bef7932e
reena47124 <reenarathode47@gmail.com>
2026-06-13 14:49:19 +0530

    update

---
+++ 32.py
@@ -0,0 +1,7 @@
+#numpy
+import numpy as np
+arr = np.random.randint(1,100,100)
+print("Mean:", np.mean(arr))
+print("Median:", np.median(arr))
+print("Variance:", np.var(arr))
+print("Std:", np.std(arr))
